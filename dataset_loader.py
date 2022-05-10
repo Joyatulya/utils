4704e48439dc0b6c1db011cec0c10d2b70fb24d3
Joyatulya <joy.atulya@gmail.com>
2022-05-10 12:21:42 +0530

    dataset_loader_bug_1

--- dataset_loader.py
+++ dataset_loader.py
@@ -1,17 +1,19 @@
 import os
 
-def load_unzip_kaggle_dataset(dataset_name):
+def load_unzip_kaggle_dataset(dataset_name,unzip_name):
 
   # Mounting Google Drive
   exec('from google.colab import drive')
   exec("drive.mount('/content/drive')")
 
   # Moving kaggle.json to the root directory
+  print('Copying kaggle.json')
   os.makedirs('/root/.kaggle',exist_ok=True)
   os.system('cp ./drive/MyDrive/kaggle.json /root/.kaggle/')
 
   # Downloading the dataset
+  print("Downloading ")
   os.system(f'kaggle datasets download -d {dataset_name}')
 
   # Unzipping to file system
-  os.system(f'{dataset_name}.zip')
+  os.system(f'{unzip_name}.zip')
